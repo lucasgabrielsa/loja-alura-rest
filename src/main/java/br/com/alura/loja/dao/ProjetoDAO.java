a991e4d7dbc109edbf9bc99a4dcec3551335cc2f
Lucas Gabriel <lucasgabriel>
2016-11-21 18:15:17 -0200

    tudo

---
+++ src/main/java/br/com/alura/loja/dao/ProjetoDAO.java
@@ -0,0 +1,34 @@
+package br.com.alura.loja.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import br.com.alura.loja.modelo.Projeto;
+
+public class ProjetoDAO {
+	
+	private static Map<Long, Projeto> banco = new HashMap();
+	private static AtomicLong contador = new AtomicLong(1);
+	
+	static {
+		banco.put(1l, new Projeto("Minha loja", 1l, 2014));
+        banco.put(2l, new Projeto("Alura", 2l, 2012));        
+	}
+	
+	public void adicona(Projeto projeto) {
+		long id = contador.incrementAndGet();
+		projeto.setId(id);
+		banco.put(id, projeto);
+	}
+	
+	public Projeto busca(long l) {
+		return banco.get(l);
+	}
+	
+	public void remove(long id) {
+		banco.remove(id);
+	}
+	
+
+}
---
+++ src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -0,0 +1,24 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ProjetoTest {
+	
+	@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/projetos").request().get(String.class);
+		System.out.println(conteudo);
+		Assert.assertTrue(conteudo.contains("Alura"));
+				
+	}
+
+
+}
